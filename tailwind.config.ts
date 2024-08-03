2c1f6b5549df9b1fa95d509bf9634dda7600f61c
MUHAMMAD OSAMA <106881591+123MuhammadOsama@users.noreply.github.com>
2024-08-03 00:59:13 -0700

    First commit

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,6 +6,8 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+  darkMode: 'class',
+  
   theme: {
     extend: {
       backgroundImage: {
